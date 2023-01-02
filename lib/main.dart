0d7cd398a73ca4b00101c9e775d4c6725596af43
mitsu9 <mitsunobu.h.9@gmail.com>
2023-01-02 21:56:32 +0900

    Show random words as list

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Startup Name Generator',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(                
           appBar: AppBar(                
-            title: const Text('Welcome to Flutter'),                
+            title: const Text('Startup Name Generator'),                
           ),
           body: const Center(
             child: RandomWords(),
@@ -45,9 +45,27 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);    
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder:  (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2; 
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
   }
 }
